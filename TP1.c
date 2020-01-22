86ef416797bf88106833dda7fa1a5342f2efb802
Niels FERREIRA <ferreiraniels@gmail.com>
2020-01-22 18:30:47 +0100

    Question 1 terminée
    décrémentation de 10 réussie.

---
+++ TP1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Debut du programme");
+	int vie = 100;
+	
+	printf("100 points de vie\n");
+	
+	printf("coup d'epee !\n");
+	
+	vie = vie - 10;
+	
+	printf("%d",vie);
+	
+}
