163bec4ad97084fbcfc903e6c052cc628bf2c6da
Niels FERREIRA <ferreiraniels@gmail.com>
2020-01-22 18:42:31 +0100

    Question 4 terminée
    Toujours alternance de coup entre 2 personnages. Ajout du choix de se protéger ou non d'une attaque. On reçoit moins de dégâts si on se protège.

--- TP1.c
+++ TP1.c
@@ -5,6 +5,7 @@ int main(){
 	
 	int vie = 100;
 	int ennemi = 50;
+	int choix = 0;
 	
 	printf("- J'ai 100HP, commencons le combat.\n\n");
 	
@@ -12,21 +13,35 @@ int main(){
 	
 		while(ennemi > 0){
 			
-			printf("coup recu !\n");
-	
-			vie = vie - 10;
-	
-			printf("Ma vie est de %d points de vie.\n\n",vie);
+			scanf("%d",&choix);
 			
+			if(choix == 1){
+				
+				printf("coup diminue !\n");
+				
+				vie = vie - 3;
+				
+				printf("Ma vie est de %d points de vie.\n\n",vie);
+				
+				choix = 0;
+			}
+			else{
+				
+				printf("coup recu !\n");
+			
+				vie = vie - 12;
+			
+				printf("Ma vie est de %d points de vie.\n\n",vie);
+			}
 			
 			
 			printf("Attaque vers l'ennemi !\n");
 			
-			ennemi = ennemi - 10;
+			ennemi = ennemi - 8;
 			
 			printf("L'ennemi a %d points de vie.\n\n",ennemi);
 			
 		}
 	
-	printf("Je t'ai battu.");
+	printf("Je t'ai battu, ca devenait tendu.");
 }
