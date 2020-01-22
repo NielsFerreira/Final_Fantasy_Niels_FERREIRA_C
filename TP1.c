39698ef5f400e54502b22d36278dadb99d9bd760
Niels FERREIRA <ferreiraniels@gmail.com>
2020-01-22 18:38:09 +0100

    Question 3 terminée
    Alternance de coups entre les adversaires

--- TP1.c
+++ TP1.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 
+
 int main(){
 	
-	printf("Debut du programme\n");
-	
 	int vie = 100;
+	int ennemi = 50;
 	
-		printf("J'ai 100HP, commencons le combat.\n");
-		
-		while(vie > 0){
-			printf("coup d'epee !\n");
+	printf("- J'ai 100HP, commencons le combat.\n\n");
+	
+	printf("- Je n'ai que 50HP, mais je peux me defendre !\n\n");
+	
+		while(ennemi > 0){
+			
+			printf("coup recu !\n");
 	
 			vie = vie - 10;
 	
-			printf("%d\n",vie);
+			printf("Ma vie est de %d points de vie.\n\n",vie);
+			
+			
+			
+			printf("Attaque vers l'ennemi !\n");
+			
+			ennemi = ennemi - 10;
+			
+			printf("L'ennemi a %d points de vie.\n\n",ennemi);
+			
 		}
 	
-	printf("\nC'etait un adversaire coriace.");
+	printf("Je t'ai battu.");
 }
