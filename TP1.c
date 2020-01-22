444d1a9dc16f9a865f2409b40b63b45f65063a99
Niels FERREIRA <ferreiraniels@gmail.com>
2020-01-22 19:22:14 +0100

    Question 5 terminée
    ajout d'une fonction qui permet à l'ennemi de soit attaquer, soit se défendre aléatoirement.

--- TP1.c
+++ TP1.c
@@ -4,45 +4,71 @@
 
 int main(){
 	
+	int aleatoire=0;
 	int vie = 100;
 	int ennemi = 50;
 	int choix = 0;
+	//int a = 0;
+	//int b = 2;
 	
-	printf("- J'ai 100HP, commencons le combat.\n\n");
-	
-	printf("- Je n'ai que 50HP, mais je peux me defendre !\n\n");
-	
-		while(ennemi > 0){
+	time(NULL);
 			
-			scanf("%d",&choix);
+			while(ennemi>0){
+				
+				scanf("%d",&choix);
+				
+				if(choix == 1){
+					
+					printf("Je bloque et prend moins de degats !\n");
+					
+					vie = vie - 3;	
+					
+					printf("Ma vie est de %d points de vie.\n",vie);
+				}
+				else{
+					
+					printf("J'ai pris un gros coups !\n");
+		
+					vie = vie - 12;
+		
+					printf("Ma vie est de %d points de vie.\n",vie);
+				}			
+
+				
+				aleatoire = rand();
+				
+				printf("%d\n",rand()%2); // écrit un chiffre aléatoire entre 0 et 1
+				
+				
+				
+				if(rand()%2==1){   // si le chiffre aléatoire est égale à 1 alors condition
+					
+					printf("l'ennemi bloque le coups !\n");
+					
+					ennemi = ennemi-2;
+					
+					printf("L'ennemi a %d points de vie.\n\n",ennemi);
+					
+				}
+				else{
+					
+					printf("\nL'ennemi prend chere !\n");
+					
+					ennemi =ennemi-8;
+				
+					printf("L'ennemi a %d points de vie.\n\n",ennemi);
+				}
+				
+				rand()%2; // permet le reset du random, placé ici, ça fonctionne comme je le souhaite.
 			
-			if(choix == 1){
-				
-				printf("coup diminue !\n");
-				
-				vie = vie - 3;
-				
-				printf("Ma vie est de %d points de vie.\n\n",vie);
-				
-				choix = 0;
 			}
-			else{
+			
+			if (vie<0){
+				ennemi=0;
 				
-				printf("coup recu !\n");
-			
-				vie = vie - 12;
-			
-				printf("Ma vie est de %d points de vie.\n\n",vie);
+				printf("\nFin du combat\n");
 			}
-			
-			
-			printf("Attaque vers l'ennemi !\n");
-			
-			ennemi = ennemi - 8;
-			
-			printf("L'ennemi a %d points de vie.\n\n",ennemi);
-			
+		
+		
+		
 		}
-	
-	printf("Je t'ai battu, ca devenait tendu.");
-}
